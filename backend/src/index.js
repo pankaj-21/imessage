5f0f406872aa6b133391ec615623d84ff98a92cd
pankaj <pankajg2146@gmail.com>
2026-07-27 20:57:17 +0530

    added docker file and build command

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,16 @@ import "dotenv/config"
 import { connectionDB } from "./lib/db.js";
 import cors from "cors";
 import {clerkMiddleware} from "@clerk/express"
+import path from "path"
+import fs from "fs"
 
 
 const app = express();
 
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json())
 app.use(cors({origin: FRONTEND_URL, credentials: true}));
@@ -17,6 +21,14 @@ app.use(clerkMiddleware());
 app.get('/health', (req, res) => {
     res.status(200).json({ok: true});
 });
+
+if(fs.existsSync(publicDir)){            //publicDir exists only incase of production
+    app.use(express.static(publicDir))  //serves built react files
+
+    app.get("/{*any}", (req, res, next) => {
+        res.sendFile(path.join(publicDir), "index.html", (err) => next(err));
+    })
+}
 
 app.listen((PORT), () => {
     connectionDB();
